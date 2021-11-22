a9898048f8a5883b0023986ead89d322d9aca68b
Luca <lucavaccari03@gmail.com>
2021-11-22 21:54:45 +0100

    Gradle configuration completed

---
+++ client/src/it/castelli/sir/ClientMain.java
@@ -0,0 +1,15 @@
+package it.castelli.sir;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ClientMain extends Application {
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+
+	}
+}
---
+++ server/src/it/castelli/sir/ServerMain.java
@@ -0,0 +1,7 @@
+package it.castelli.sir;
+
+public class ServerMain {
+	public static void main(String[] args) {
+
+	}
+}
