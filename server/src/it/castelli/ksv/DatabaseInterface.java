c436783798c6519ed169bdc8a7801348f18865c2
Luca <lucavaccari03@gmail.com>
2021-12-06 12:18:13 +0100

    Implemented GET /authors and GET /topics (but the query is still on the server, instead of the SQL)

--- server/src/it/castelli/ksv/DatabaseInterface.java
+++ server/src/it/castelli/ksv/DatabaseInterface.java
@@ -2,27 +2,27 @@ package it.castelli.ksv;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 /**
- * Class to interact with the MySQL database.
- * initialize() should be called before using this class.
+ * Class to interact with the MySQL database. initialize() should be called before using this class.
  */
 public final class DatabaseInterface {
-    private static final String DB_URL = "jdbc:mysql://localhost/ksv-links";
-    private static final String DB_USERNAME = "root";
+	private static final String DB_URL = "jdbc:mysql://localhost/ksv-links";
+	private static final String DB_USERNAME = "root";
 
-    private static Connection sqlConnection;
+	private static Connection sqlConnection;
 
-    private DatabaseInterface() {
-    }
+	private DatabaseInterface() {
+	}
 
-    /**
-     * Sets the defaults of the class.
-     */
-    public static void initialize() {
-        // TEST
+	/**
+	 * Sets the defaults of the class.
+	 */
+	public static void initialize() {
+		// TEST
 //        ArrayList<Opus> opuses = new ArrayList<>();
 //        addData(new Opus("I malavoglia", new Date(1890, 4, 23), "prosa", "verismo", "I malavoglia"
 //        ));
@@ -36,45 +36,77 @@ public final class DatabaseInterface {
 //                "Mondo"));
 //        addData(new Topic("Decadentismo", new Date(1860, 1, 1), new Date(1910, 4, 6), "Corrente culturale " +
 //                "decadentista", "Europa"));
-        // END TEST
+		// END TEST
 
-        try {
-            sqlConnection = DriverManager.getConnection(DB_URL + "?user=" + DB_USERNAME);
-            System.out.println("Connected to " + DB_URL + " as " + DB_USERNAME);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-    }
+		try {
+			sqlConnection = DriverManager.getConnection(DB_URL + "?user=" + DB_USERNAME);
+			System.out.println("Connected to " + DB_URL + " as " + DB_USERNAME);
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
-    /**
-     * Provides an array of all authors in the database.
-     *
-     * @return An array of the authors of the database.
-     */
-    public static Author[] getAllAuthors() {
-        ArrayList<Author> authorList = new ArrayList<>();
+	/**
+	 * Provides an array of all authors in the database.
+	 *
+	 * @return An array of the authors of the database.
+	 */
+	public static Author[] getAllAuthors() {
+		ArrayList<Author> authorList = new ArrayList<>();
+		try {
+			ResultSet result = sqlConnection.createStatement()
+					.executeQuery("select * from autori");
+			while (result.next()) {
+				authorList.add(new Author(
+						result.getString("nome"),
+						result.getString("cognome"),
+						result.getDate("data_nascita"),
+						result.getDate("data_morte"),
+						result.getString("vita")
+				));
+			}
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return authorList.toArray(new Author[0]);
+	}
 
-        return authorList.toArray(new Author[0]);
-    }
+	/**
+	 * Provides an array of all topics in the database.
+	 *
+	 * @return An array of the topics of the database.
+	 */
+	public static Topic[] getAllTopics() {
+		ArrayList<Topic> topicList = new ArrayList<>();
 
-    /**
-     * Provides an array of all topics in the database.
-     *
-     * @return An array of the topics of the database.
-     */
-    public static Topic[] getAllTopics() {
-        ArrayList<Topic> topicList = new ArrayList<>();
+		try {
+			ResultSet result = sqlConnection.createStatement().executeQuery("select * from argomenti");
+			while (result.next()) {
+				topicList.add(new Topic(
+						result.getString("nome"),
+						result.getDate("data_inizio"),
+						result.getDate("data_fine"),
+						result.getString("descrizione"),
+						result.getString("luogo")
+				));
+			}
 
-        return topicList.toArray(new Topic[0]);
-    }
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return topicList.toArray(new Topic[0]);
+	}
 
-    /**
-     * Replaces an entity in the database.
-     *
-     * @param oldE The old entity which will be replaced by the new one.
-     * @param newE The new entity which will replace the old one.
-     */
-    public static void modifyData(Entity oldE, Entity newE) {
+	/**
+	 * Replaces an entity in the database.
+	 *
+	 * @param oldE The old entity which will be replaced by the new one.
+	 * @param newE The new entity which will replace the old one.
+	 */
+	public static void modifyData(Entity oldE, Entity newE) {
 
-    }
+	}
 }
